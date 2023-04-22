4304be85449637bdacc1e110b7c38007885e136b
KyleRiebeling <125304972+KyleRiebeling@users.noreply.github.com>
2023-04-21 18:06:19 -0700

    Transferred over old data from java poker program. Also included the start from my first choice of this project, blackjack.

---
+++ Card.h
@@ -0,0 +1,46 @@
+/*
+ * File:   Card.h
+ * Author: Kyle
+ *
+ * Created on April 21, 2023, 5:04 PM
+ */
+
+#ifndef CARD_H
+#define CARD_H
+#include <string>
+
+using namespace std;
+
+class Card{
+private:
+   string face;
+   string suit;
+public:
+
+   Card(){
+      face = "Face";
+      suit = "Suit";
+   }
+   string toString() { 
+      return face + " of " + suit; 
+   }
+
+   string getFace(){
+      return face;
+   }
+
+   string getSuit(){
+      return suit;
+   }
+   
+   void setFace(string f){
+      face = f;
+   }
+
+   void setSuit(string s){
+      suit = s;
+   }
+};
+
+#endif /* CARD_H */
+
--- main.cpp
+++ main.cpp
@@ -1,24 +1,90 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
- */
-
-/* 
- * File:   main.cpp
- * Author: Kyle
- *
- * Created on April 21, 2023, 5:00 PM
- */
-
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <list>
+#include <queue>
 #include <cstdlib>
+
+#include "DeckOfCards.h"
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+void printPlayers(map<string,int> a){
+    if (a.size() == 0){
+        cout << "This table is empty!" << endl << endl;
+        return;
+    }
+    cout << "Player   Bank" << endl;
+    cout << "-------------" << endl;
+    for (auto pair : a){
+    cout << pair.first << " - $";
+    cout << pair.second << endl;
+    }
+    cout << endl;
+}
 
-   return 0;
+int main() {
+    srand(time(0));
+    map<string, int> players;
+    map<string, list<int>> playerStats;
+    list<int> player1Stats;
+    list<int> player2Stats;
+    list<int> player3Stats;
+
+    int numPlayers = 0;
+    int tempN;
+    string tempS;
+
+    cout << "Welcome to the casino! How many people will be playing today? (Max of 3)" << endl;
+    cin >> numPlayers;
+    
+    while (numPlayers <= 0 || cin.fail()){//Error check for amount of players
+        cout << "Must be at least one player. Enter a number from 1-3: ";
+        cin >> numPlayers;
+    }
+   cout << "Enter starting cash for all players: ";
+      cin >> tempN;
+
+    for (int i = 0; i < numPlayers; i++){ //Setting up player maps
+        cout << "Enter player " << i+1 << "'s name: ";
+        cin >> tempS;
+        players.insert(pair<string,int>(tempS,tempN));
+        if (i == 0){
+            playerStats.insert(pair<string,list<int>>(tempS,player1Stats));
+            player1Stats.push_back(tempN);
+        }
+        if (i == 1){
+            playerStats.insert(pair<string,list<int>>(tempS,player2Stats));
+            player2Stats.push_back(tempN);
+        }
+        if (i == 2){
+            playerStats.insert(pair<string,list<int>>(tempS,player3Stats));
+            player3Stats.push_back(tempN);
+        }
+    }
+    
+    cout << endl <<"Welcome to the table!" << endl;
+    printPlayers(players);
+
+    //-----GAME LOOP-----
+
+    //Place Bets
+
+    //Deal cards
+
+    //Simulate other players turns
+
+    
+    DeckOfCards myDeckOfCards;
+    myDeckOfCards.shuffle(); // place Cards in random order
+    Card playersHand[5];
+    
+    for (int i = 0; i < 5; i++) {
+      playersHand[i] = myDeckOfCards.dealCard();
+      cout << playersHand[i].toString() << endl;
+      }
+    
+    cout << myDeckOfCards.evaluateHand(playersHand);
+    return 0;
 }
 
