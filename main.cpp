dbae256355c4e871642ffead5c5e8489c1d1a2bd
Kyle Riebeling <kriebeling1@student.rccd.edu>
2023-04-23 12:22:51 -0700

    Fixed error check loop for numPlayers

--- main.cpp
+++ main.cpp
@@ -10,22 +10,16 @@ using namespace std;
 
 
 int main() {
-   srand(time(0));
-   map<string, int> players;
-   map<string, list<int>> playerStats;
-   list<int> player1Stats;
-   list<int> player2Stats;
-   list<int> player3Stats;
-
    int numPlayers = 0;
-   int tempN;
-   string tempS;
 
    cout << "Welcome to the casino! How many people will be playing today? (Max of 3)" << endl;
    cin >> numPlayers;
 
-   while (numPlayers <= 0 || numPlayers >= 4) {//Error check for amount of players
+   //Error check for amount of players
+   while (numPlayers <= 0 || numPlayers >= 4 || cin.fail()) {
       cout << "Must be at least one player. Enter a number from 1-3: ";
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(),'\n');
       cin >> numPlayers;
    }
    
@@ -36,20 +30,33 @@ int main() {
 
    //-----GAME LOOP-----
 
-   //Place Bets
+   //Place Buy-in Bets
 
    //Deal cards
 
-   //Simulate other players turns
+   //Ask for more bets
+
+   //Dealer places three cards on table
+
+   //Ask for more bets/folds
+
+   //Dealer places one more card
+
+   //Ask for bets/fold
+
+   //Dealer places last card
+
+   //Last round of betting
+
+   //Reveal winner and their hand
+
+   //Ask if you want to keep going
 
 
    DeckOfCards myDeckOfCards;
    myDeckOfCards.shuffleDeck(); // place Cards in queue
    Card playersHands[numPlayers][2];
-   myDeckOfCards.dealHands(playersHands,numPlayers);
-   /*for(int i = 0; i < numPlayers; i++){
-      cout << myDeckOfCards.evaluateHand(playersHand[j]) << endl << endl;
-   }*/
+   myDeckOfCards.dealHands(playersHands,numPlayers);//Give each player 2 cards
 
    
    return 0;
